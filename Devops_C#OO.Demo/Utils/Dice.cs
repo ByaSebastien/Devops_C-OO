48166c2c9b8b2b2651c731d0e67bf0d5fa3d6410
technifutur <technifutur@DESKTOP-EHN422D>
2023-03-17 08:37:20 +0100

    Lancé de dé

--- Devops_C#OO.Demo/Utils/Dice.cs
+++ Devops_C#OO.Demo/Utils/Dice.cs
@@ -18,5 +18,15 @@ namespace Devops_C_OO.Demo.Utils
     }
     public static class Dice
     {
+        public static int Throws(DiceType dice, int nbThrow = 1,int nbToKeep = 1)
+        {
+            Random rnd = new Random();
+            List<int> throws = new List<int>();
+            for(int i = 0; i < nbThrow; i++)
+            {
+                throws.Add(rnd.Next((int)dice) + 1);
+            }
+            return throws.OrderByDescending(x => x).Take(nbToKeep).Sum();
+        }
     }
 }
